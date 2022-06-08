ae37ec1b75dea7fc59d79c4bcb3ac4cee8d634e9
isaac <gilman.isaac@gmail.com>
2022-06-07 21:02:00 -0400

    working on header a bit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -27,7 +28,8 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
