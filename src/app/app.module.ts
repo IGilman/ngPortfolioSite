323a8084cd339521cfcd89b3884f3aabd144967a
isaac <gilman.isaac@gmail.com>
2022-06-14 20:54:13 -0400

    got basic side nav working, leaving comments in for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 
 
@@ -29,7 +30,8 @@ import { MatButtonModule } from '@angular/material/button';
     FlexLayoutModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
