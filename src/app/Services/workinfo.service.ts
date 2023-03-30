f8affc1bd1e9947b9bdd203f85fde7ca61c37193
isaac <gilman.isaac@gmail.com>
2023-03-29 21:11:10 -0400

    adding more dynamic data binding and cleaning up some scss

--- src/app/Services/workinfo.service.ts
+++ src/app/Services/workinfo.service.ts
@@ -16,6 +16,7 @@ export class WorkinfoService {
       "title": "Job One",
       "subtitle": "small description of job test",
       "image": "https://material.angular.io/assets/img/examples/shiba2.jpg",
+      "quickDescription": "quick description of job 1",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       "projectUrl": "https://www.freecodecamp.org/",
       "linkToProjectText": "Link to the project 1"
@@ -24,6 +25,7 @@ export class WorkinfoService {
       "title": "Job two",
       "subtitle": "blahhhhhhhhhhhhhhhhhhhhhhh test",
       "image": "https://material.angular.io/assets/img/examples/shiba2.jpg",
+      "quickDescription": "quick description of job 2",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At tellus at urna condimentum mattis pellentesque id. Ac turpis egestas integer eget aliquet. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Vitae ultricies leo integer malesuada nunc vel. Morbi non arcu risus quis varius quam quisque. Risus at ultrices mi tempus imperdiet nulla. Sem viverra aliquet eget sit amet tellus cras adipiscing. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Venenatis cras sed felis eget velit aliquet sagittis. ",
       "projectUrl": "https://www.freecodecamp.org/",
       "linkToProjectText": "Link to the Project 2"
@@ -32,6 +34,7 @@ export class WorkinfoService {
       "title": "Job three",
       "subtitle": "j;dljjfd test",
       "image": "https://material.angular.io/assets/img/examples/shiba2.jpg",
+      "quickDescription": "quick description of job 3",
       "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At tellus at urna condimentum mattis pellentesque id. Ac turpis egestas integer eget aliquet. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Vitae ultricies leo integer malesuada nunc vel. Morbi non arcu risus quis varius quam quisque. Risus at ultrices mi tempus imperdiet nulla. Sem viverra aliquet eget sit amet tellus cras adipiscing. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Venenatis cras sed felis eget velit aliquet sagittis. ",
       "projectUrl": "https://www.freecodecamp.org/",
       "linkToProjectText": "Link to the Project 3"
--- src/app/workexperiencecard/workexperiencecard.component.ts
+++ src/app/workexperiencecard/workexperiencecard.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-workexperiencecard',
   templateUrl: './workexperiencecard.component.html',
   styleUrls: ['./workexperiencecard.component.scss']
 })
-export class WorkexperiencecardComponent  {
+export class WorkexperiencecardComponent implements OnInit {
 
-  @Input() title = "title test"
-  @Input() subtitle = "subtitle test"
-  @Input() image = "https://material.angular.io/assets/img/examples/shiba2.jpg"
-  @Input() description = "description test"
+  @Input() componentJob: any;
+  title: any;
+  subtitle: any;
+  image: any;
+  quickDescription: any;
+
+
+  ngOnInit() {
+    console.log(this.componentJob)
+    this.title = this.componentJob.title;
+    this.subtitle = this.componentJob.subtitle;
+    this.image = this.componentJob.image
+    this.quickDescription = this.componentJob.quickDescription
+  }
 
 }
