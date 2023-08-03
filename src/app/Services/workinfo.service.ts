9965717d99158b9ef850fe206ea2548b2267875d
Isaac Gilman <gilman.isaac@gmail.com>
2023-08-02 22:12:36 -0400

    adding amrock project details

--- src/app/Services/workinfo.service.ts
+++ src/app/Services/workinfo.service.ts
@@ -13,35 +13,43 @@ export class WorkinfoService {
 
   jobs: any = [
     {
-      "title": "Amrock Connect Portal",
+      "title": "Amrock - Customer Portal",
       "jobname": "jobone",
-      "subtitle": "Portal for users to complete to-dos and upload docs in the Title process",
+      "subtitle": "Portal for agents and customers to complete their Title and Closing tasks",
       "image": "https://material.angular.io/assets/img/examples/shiba2.jpg",
-      "quickDescription": "Self-service customer portal for users to complete their Title and Closing process",
-      "description": "",
-      "projectUrl": "https://www.freecodecamp.org/",
-      "linkToProjectText": "Link to the project 1"
+      "quickDescription": "Portal for agents and customers to complete their Title and Closing tasks",
+      "description": {
+        "descripparone": "Amrock’s portal is a unique attempt to increase efficiency in the Title and Closing processes by providing users a self-service tool to complete tasks that traditionally require email and phone calls. My focus has been on the entire technology stack, with a passion for making our UI/UX more aligned to our users needs.",
+        "descrippartwo": "I bring expertise to our front-end when most engineers focus on backend services. I work closely with our Product Owner to make sure our site is designed with a mobile-first approach and has a seamless responsive design for any form factor. I maintain a custom Sass and Angular Material library that is implemented globally across our frontend Nx Monorepo. I also maintain our NgXs (similar to NgRx) store pattern, while pushing a reactive programming approach across the entire Angular application.",
+        "descripparthree": "Also, I regularly work in our .NET backend. I help maintain and add functionality to one of our seven services that powers our front-end. Our externally exposed services are built with a controller-service-repository pattern to separate concerns and provide clear responsibility at each layer, while our non-exposed services communicate via a Command/Event architecture where we use NServiceBus and RabbitMQ to facilitate messaging."
+      },
+      "stack": {
+        "frontend": "Angular",
+        "backend": "C#/.NET 6",
+        "database": "MS SQL",
+      },
+      "projectUrl": "https://www.amrock.com/login",
+      "linkToProjectText": "Amrock - Real Estate Agents and Customer Portal"
     },
     {
       "title": "Farebox",
       "jobname": "jobtwo",
-      "subtitle": "blahhhhhhhhhhhhhhhhhhhhhhh test",
+      "subtitle": "Booking and scheduling software for bus fleets",
       "image": "https://material.angular.io/assets/img/examples/shiba2.jpg",
-      "quickDescription": "Booking and scheduling software for bus fleets ",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At tellus at urna condimentum mattis pellentesque id. Ac turpis egestas integer eget aliquet. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Vitae ultricies leo integer malesuada nunc vel. Morbi non arcu risus quis varius quam quisque. Risus at ultrices mi tempus imperdiet nulla. Sem viverra aliquet eget sit amet tellus cras adipiscing. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Venenatis cras sed felis eget velit aliquet sagittis. ",
+      "quickDescription": "Booking and scheduling software for bus fleets",
+      "description": {
+        "descripparone" : "",
+        "descrippartwo": "",
+        "descripparthree": "",
+      },
+      "stack": {
+        "frontend": "Vue.js",
+        "backend": "Node.js with Express",
+        "database": "Mongo DB",
+      },
       "projectUrl": "https://www.freecodecamp.org/",
       "linkToProjectText": "Link to the Project 2"
-    },
-    {
-      "title": "Job three",
-      "jobname": "jobthree",
-      "subtitle": "j;dljjfd test",
-      "image": "https://material.angular.io/assets/img/examples/shiba2.jpg",
-      "quickDescription": "quick description of job 3",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At tellus at urna condimentum mattis pellentesque id. Ac turpis egestas integer eget aliquet. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Vitae ultricies leo integer malesuada nunc vel. Morbi non arcu risus quis varius quam quisque. Risus at ultrices mi tempus imperdiet nulla. Sem viverra aliquet eget sit amet tellus cras adipiscing. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Venenatis cras sed felis eget velit aliquet sagittis. ",
-      "projectUrl": "https://www.freecodecamp.org/",
-      "linkToProjectText": "Link to the Project 3"
-    },
+    }
   ]
 
   //./src/assets/workdetials.json'
